821f3283f60db491e292901da86c358b08b649a5
kuochi <lukeafang@gmail.com>
2019-06-21 16:20:01 -0400

    Add requireAdministrator to run application

--- ServicesInstaller/wInstaller.cs
+++ ServicesInstaller/wInstaller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,14 @@ namespace ServicesInstaller
 {
     public partial class wInstaller : Form
     {
+        private List<string> _serviceNameList;
+        private string _selectServiceName;
+
         public wInstaller()
         {
             InitializeComponent();
+            _selectServiceName = "";
+
         }
 
         private void cbServiceName_SelectedIndexChanged(object sender, EventArgs e)
@@ -39,11 +45,48 @@ namespace ServicesInstaller
             string version = System.Windows.Forms.Application.ProductVersion;
             this.lblInstallerVersion.Text = String.Format("Version:{0}", version);
 
+            _serviceNameList = getServiceFileNames();
+
+            cbServiceName.Items.Clear();
+            foreach (string serviceName in _serviceNameList)
+            {
+                cbServiceName.Items.Add(serviceName);
+            }
+            //select first one
+            if (cbServiceName.Items.Count > 0)
+            {
+                cbServiceName.SelectedIndex = 0;
+                _selectServiceName = cbServiceName.Items[0].ToString();
+            }
+
             updateUIValue();
+        }
+
+        private List<string> getServiceFileNames()
+        {
+            List<string> serviceNames = new List<string>();
+            string folderPath = Environment.CurrentDirectory;
+            foreach (string f in Directory.GetFiles(folderPath))
+            {
+                string fileName = Path.GetFileNameWithoutExtension(f);
+                string fileExtension = Path.GetExtension(f);
+
+                if (fileExtension.CompareTo(".exe") != 0) { continue; }
+                else{
+                    string tempExtension = Path.GetExtension(fileName);
+                    if (tempExtension.Length != 0) { continue; }
+
+                    if (fileName.CompareTo("ServicesInstaller") == 0) { continue; }
+
+                    serviceNames.Add(fileName);
+                }
+            }
+            return serviceNames;
         }
 
         private void updateUIValue()
         {
+
 
         }
     }
